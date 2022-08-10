664b6aea044511e66df3d00dc47f9ebd553b9bb6
shubhamskathare <89640618+shubhamskathare@users.noreply.github.com>
2022-08-10 12:54:26 +0530

    Add files via upload

---
+++ HelloWorld-spring/src/main/java/com/mindtree/helloworld/HelloWorldSpringApplication.java
@@ -0,0 +1,14 @@
+package com.mindtree.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(HelloWorldSpringApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ HelloWorld-spring/src/test/java/com/mindtree/helloworld/HelloWorldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
